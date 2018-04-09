fb6795373380cd99f39f6e84088523a1c963a14f
Babazile <mpongozababazile@gmail.com>
2018-04-09 10:32:56 +0200

    Added test for bootcamp functions

---
+++ test/fromWhere.test.js
@@ -0,0 +1,8 @@
+describe('fromWhere' , function(){
+    it('should state where the regNo is from', function(){
+        assert.equal(fromWhere('Paarl'), 0);
+    });
+    it('should return Paarl regNo', function(){
+        assert.equal(fromWhere('Paarl'), 0);
+    });
+});
