b7a2d9ba8d4566f5df87f4723262a54d4c78bc70
Babazile <mpongozababazile@gmail.com>
2018-04-10 09:39:01 +0200

    made few changes in bootcamp-tests

--- test/regCheck.test.js
+++ test/regCheck.test.js
@@ -1,14 +1,14 @@
 describe('regCheck' , function(){
     it('it should return true if the regNo is from Gauteng' , function(){
-        assert.equal(regCheck('12254 GP'),true);
+        assert.equal(regCheck('12254 GP', 'GP'),true);
     });
     it('it should return true if the regNo is from Limpopo' , function(){
-        assert.equal(regCheck('189-012 L'), true);
+        assert.equal(regCheck('189-012 L', 'L'), true);
     });
     it('it should return true if the regNo is from Eastern  Cape' , function(){
-        assert.equal(regCheck('75757 EC'), true);
+        assert.equal(regCheck('75757 EC', 'EC'), true);
     });
     it('it should return true if the regNo is from Durban' , function(){
-        assert.equal(regCheck('67566 ND'), true);
+        assert.equal(regCheck('67566 ND', 'ND'), true);
     });
 });
--- test/yearsAgo.test.js
+++ test/yearsAgo.test.js
@@ -1,9 +1,9 @@
 describe('yearsAgo' , function(){
     it('should state how many years ago' , function(){
-        assert.equal(yearsAgo("1994"),24);
+        assert.equal(yearsAgo(1994),24);
     });
-    it('should return ' , function(){
-        assert.equal(yearsAgo(""),2);
+    it('should state the current year' , function(){
+        assert.equal(yearsAgo(2018),0);
     });
 
 });
